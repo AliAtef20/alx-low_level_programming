43030c80aff6f0c11ac0e0e75f4e676832e5468a
AliAtef20 <aliatef20@gmail.com>
2023-04-03 18:02:49 -0700

    100-set_string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of pointer a char
+ * @s: pointer to pointer
+ * @to: pointer char
+ * Return: 0
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
