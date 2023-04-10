dde09a0112dcb70d5efc03e0ff1e9aeec47ac2b7
AliAtef20 <aliatef20@gmail.com>
2023-04-09 17:04:04 -0700

    libmy

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - function that computes the absolute
+ * vlaue of an integer
+ * @n: takes in integer type inpute in function
+ * Return: alwys 0
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		n = (-1) * n;
+	return (n);
+}
