642514be166098ffce72d15897c2377a4a91b459
AliAtef20 <aliatef20@gmail.com>
2023-04-14 16:04:20 -0700

    2-calloc

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -8,8 +8,7 @@ void _puts(char *str);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
-char *_memset(char *s, char b, unsigned int n);
-
+int *array_range(int min, int max);
 
 
 #endif
