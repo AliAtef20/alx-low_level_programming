045df4165cb0ec6aa3075fa463802810c73309bf
AliAtef20 <aliatef20@gmail.com>
2023-04-14 15:52:15 -0700

    2-calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -29,6 +29,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	m = malloc(sizeof(int) * nmemb);
 	if (m == 0)
 		return (NULL);
-	_memset((m, 0, sizeof(int) * memb);
+	_memset(m, 0, sizeof(int) * nmemb);
 			return (m);
 }
