d4ef1c95951c08bf14ebe2e6ab5eefecd38c67d6
AliAtef20 <aliatef20@gmail.com>
2023-04-14 16:27:42 -0700

    2-calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,10 +1,10 @@
-#include "main.h"
+i#include "main.h"
 /**
  * *_memset - fills memory with a constant byte
  * @s: pointer
  * @b: constant
  * @n: max ibytes to use
- * Return: s
+ * Return: ptr
  */
 char *_memset(char *s, char b, unsigned int n)
 {
