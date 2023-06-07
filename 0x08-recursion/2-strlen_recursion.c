74ddddabcf9ee5572a1db1db194027006e46aca2
AliAtef20 <aliatef20@gmail.com>
2023-06-07 06:11:44 -0700

    2-strlen_recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,10 +6,11 @@
  */
 int _strlen_recursion(char *s)
 {
-	int ntr = 0;
-	if (*s > 0)
-	{
-		ntr += _strlen_recursion(s + 1) + 1;
-	}
-	return (ntr);
+
+int n = 0;
+if (*s > 0)
+{
+	n += _strlen_recursion(s + 1) + 1;
+}
+return (n);
 }
